bda53245aa550f9c2070b03c2e26ffa9d73f342e
unknown <aloha.hat.2508@gmail.com>
2021-01-10 23:05:28 +0700

    first commit

---
+++ src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,22 @@
+package com.example.demo.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Data
+public class User {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+    private String fullName;
+    @ManyToMany
+    private Set<Role> roles;
+    public User() {
+    }
+
+}
---
+++ src/main/java/com/example/demo/service/user/UserServiceImpl.java
@@ -0,0 +1,45 @@
+package com.example.demo.service.user;
+
+import com.example.demo.model.User;
+import com.example.demo.repository.IUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl implements IUserService{
+    @Autowired
+    private IUserRepository userRepository;
+    @Override
+    public Iterable<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public Optional<User> findById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    @Override
+    public void remove(Long id) {
+        userRepository.deleteById(id);
+    }
+
+    @Override
+    public User findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
+}
